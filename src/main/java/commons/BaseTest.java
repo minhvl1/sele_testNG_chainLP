943e35709558bd3f4de23fa7cf649b51043abd97
minhvl1 <minhvu890yahoo@gmail.com>
2023-04-05 10:48:47 +0700

    implement manage browser

--- src/main/java/commons/BaseTest.java
+++ src/main/java/commons/BaseTest.java
@@ -8,73 +8,98 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 import org.testng.annotations.AfterTest;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
+
     protected WebDriver getBrowserDriver(String browserName) {
-       switch (browserName){
-           case "firefox":
-               WebDriverManager.firefoxdriver().setup();
-               FirefoxOptions firefoxOptions = new FirefoxOptions();
-               driver = new FirefoxDriver(firefoxOptions);
-               break;
+        if (driver == null) {
+            try {
+                switch (browserName) {
+                    case "firefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        driver = new FirefoxDriver(firefoxOptions);
+                        break;
 
-           case "hfirefox":
-               WebDriverManager.firefoxdriver().setup();
-               FirefoxOptions hfirefoxOptions = new FirefoxOptions();
-               hfirefoxOptions.addArguments("--headless");
-               driver = new FirefoxDriver(hfirefoxOptions);
-               break;
+                    case "hfirefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        FirefoxOptions hfirefoxOptions = new FirefoxOptions();
+                        hfirefoxOptions.addArguments("--headless");
+                        driver = new FirefoxDriver(hfirefoxOptions);
+                        break;
 
-           case "chrome":
-               WebDriverManager.chromedriver().setup();
-               ChromeOptions options = new ChromeOptions();
-               options.addArguments("start-maximized");
-               options.addArguments("--remote-allow-origins=*");
-               driver = new ChromeDriver(options);
-               break;
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("start-maximized");
+                        options.addArguments("--remote-allow-origins=*");
+                        driver = new ChromeDriver(options);
+                        break;
 
-           case "hchrome":
-               WebDriverManager.chromedriver().setup();
-               ChromeOptions chromeOptionsoptions = new ChromeOptions();
-               chromeOptionsoptions.addArguments("--headless");
-               chromeOptionsoptions.addArguments("--disable-dev-shm-usage");
-               chromeOptionsoptions.addArguments("--no-sandbox");
-               chromeOptionsoptions.addArguments("--remote-allow-origins=*");
-               driver = new ChromeDriver(chromeOptionsoptions);
-               break;
+                    case "hchrome":
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions chromeOptionsoptions = new ChromeOptions();
+                        chromeOptionsoptions.addArguments("--headless");
+                        chromeOptionsoptions.addArguments("--disable-dev-shm-usage");
+                        chromeOptionsoptions.addArguments("--no-sandbox");
+                        chromeOptionsoptions.addArguments("--remote-allow-origins=*");
+                        driver = new ChromeDriver(chromeOptionsoptions);
+                        break;
 
-           case "hedge":
-               WebDriverManager.edgedriver().setup();
-               EdgeOptions hegdeoptions = new EdgeOptions();
-               hegdeoptions.addArguments("--headless");
-               driver = new EdgeDriver(hegdeoptions);
-               break;
+                    case "hedge":
+                        WebDriverManager.edgedriver().setup();
+                        EdgeOptions hegdeoptions = new EdgeOptions();
+                        hegdeoptions.addArguments("--headless");
+                        driver = new EdgeDriver(hegdeoptions);
+                        break;
 
-           case "edge":
-               WebDriverManager.edgedriver().setup();
-               EdgeOptions edgeOptions = new EdgeOptions();
-               edgeOptions.addArguments("start-maximized");
-               driver = new EdgeDriver(edgeOptions);
-               break;
+                    case "edge":
+                        WebDriverManager.edgedriver().setup();
+                        EdgeOptions edgeOptions = new EdgeOptions();
+                        edgeOptions.addArguments("start-maximized");
+                        driver = new EdgeDriver(edgeOptions);
+                        break;
 
-           default:
+                    default:
 //                        driver = new FirefoxDriver();
-               driver = new EdgeDriver();
-               break;
+                        driver = new EdgeDriver();
+                        break;
 
-       }
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//        driver.get("https://www.google.com/");
+                }
+            } finally {
+                Runtime.getRuntime().addShutdownHook(new Thread(new BrowserCleanup()));
+            }
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+        }
         return driver;
     }
 
     @AfterTest
-    public void TearDown(){
+    public void TearDown() {
         driver.close();
+    }
+
+    private class BrowserCleanup implements Runnable {
+        @Override
+        public void run() {
+            close();
+        }
+    }
+
+    public void close() {
+        try {
+            if (driver != null) {
+                driver.quit();
+            }
+        } catch (UnreachableBrowserException e) {
+            System.out.println("Can not close the browser");
+        }
     }
 
 }
